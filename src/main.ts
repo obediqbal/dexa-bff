03307d48165964f961025e3b26c724adf6360bc4
Rezapu Obed <obed.ubaidillah@gmail.com>
2026-01-04 18:55:39 +0700

    refactor: use http only cookie instead of auth header

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
   const configService = app.get(ConfigService);
   const logger = new Logger('Bootstrap');
 
